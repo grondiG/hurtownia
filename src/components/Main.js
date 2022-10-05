829e9d2778ace54b85e205b8554ff8c4a5933fc4
grondi <arogrondi@gmail.com>
2022-10-05 16:05:28 +0200

    first commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom"
 const Main = () => {
     return (
         <>
-            <img src="https://m.media-amazon.com/images/I/41vcHNN5TPL._AC_SY580_.jpg" alt='asd' />
             <div>
                 <Link to="/login"><input type="button" value="Logowanie" /></Link>
                 <Link to="/register"><input type="button" value="Rejestracja" /></Link>
